cbde9af130ad8483d21d2a4120a8c7394a109206
Som Jadhav <somjadhav787@gmail.com>
2021-03-18 12:15:11 -0500

    worked on dashboard layout

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,11 @@ app = dash.Dash(__name__)
 df = get_data()
 df.reset_index(inplace=True)
 
-options_dict = {}
+options_list = []
+for index, row in df.iterrows():
+    county_dict = {"label":row['County Name'], "value":row['County Name']}
+    options_list.append(county_dict)
+
 
 
 # App layout
@@ -21,7 +25,31 @@ app.layout = html.Div([
     html.H1('Texas Corovirus Cases Dashboard', style={'text-align':'center'}),
 
     dcc.Dropdown(id='select-county',
-            options = [
-                
-            ])
+            options = options_list,
+            multi=False,
+            value="Total",
+            style={"width":"50%"}
+            ),
+    
+    html.Br(),
+
+    dcc.Graph(id='cum_cases_graph', figure={}),
+    
+    html.Br(),
+
+    dcc.Graph(id='new_cases_graph', figure={})
+  
 ])
+
+@app.callback(
+    [Output(component_id='cum_cases_graph', component_property='figure'),
+    Output(component_id='new_cases_graph', component_property='figure')],
+    [Input(component_id='select-county', component_property='value')]
+)
+def update_graph(county_selected):
+
+    df_copy = df.copy()
+    df_copy = df_copy[df_copy['County Name'] == county_selected]
+
+    df_new_cases = df_copy.copy()
+    
