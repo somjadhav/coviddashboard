5684dd142b7ae78fd9a9c5ac7bd0d432c1b83172
Som Jadhav <somjadhav787@gmail.com>
2021-03-18 21:39:33 -0500

    created graphs

--- dashboard.py
+++ dashboard.py
@@ -10,11 +10,10 @@ from data import get_data
 app = dash.Dash(__name__)
 
 df = get_data()
-df.reset_index(inplace=True)
 
 options_list = []
-for index, row in df.iterrows():
-    county_dict = {"label":row['County Name'], "value":row['County Name']}
+for name in list(df.columns)[1:]:
+    county_dict = {"label":str(name), "value":str(name)}
     options_list.append(county_dict)
 
 
@@ -22,13 +21,13 @@ for index, row in df.iterrows():
 # App layout
 app.layout = html.Div([
 
-    html.H1('Texas Corovirus Cases Dashboard', style={'text-align':'center'}),
+    html.H1('Texas Coronavirus Cases Dashboard', style={'text-align':'center'}),
 
     dcc.Dropdown(id='select-county',
             options = options_list,
             multi=False,
             value="Total",
-            style={"width":"50%"}
+            style={"width":"35%"}
             ),
     
     html.Br(),
@@ -47,9 +46,64 @@ app.layout = html.Div([
     [Input(component_id='select-county', component_property='value')]
 )
 def update_graph(county_selected):
-
+    
     df_copy = df.copy()
-    df_copy = df_copy[df_copy['County Name'] == county_selected]
+    df_copy['Diff'] = df_copy[county_selected].diff().fillna(df_copy[county_selected])
+    df_copy
 
-    df_new_cases = df_copy.copy()
+    if county_selected == 'Total':
+        title_str_1 = "Cumulative Cases for Texas"
+        title_str_2 = "Daily New Cases for Texas"
+        
+
+    else:
+        title_str_1 = "Cumulative Cases for " + county_selected + " County"
+        title_str_2 = "New Cases for " + county_selected + " County"
+
+    fig1 = px.line(data_frame=df, x='Date', y=county_selected, 
+        title=title_str_1,
+        labels={'Date':"Date", county_selected:"# of Cases"},
+        hover_data=[county_selected]
+    )
+
+    fig1.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=3, label="3m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(label="All", step="all")
+            ])
+        )
+    )
+    
+    fig2 = px.line(data_frame=df_copy, x='Date', y='Diff',
+        title=title_str_2,
+        labels={'Date':"Date", 'Diff':"# of Cases"},
+        hover_data=['Diff']
+    )
+
+    fig2.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=3, label="3m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(label="All", step="all")
+            ])
+        )
+    )
+
+    return fig1, fig2
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
     
--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
+import datetime
 
 '''
 def get_data():
@@ -18,8 +19,18 @@ def get_data():
 def get_data():
     df = pd.read_csv('datafile.csv')
 
-    print(df.head())
-
     df = df.drop(['Cases 03-04-2020', 'Cases 03-05-2020'], axis=1)
+    df_dates = []
+    for val in list(df.columns)[1:]:
+        time_str = val[6:]
+        time = datetime.datetime.strptime(time_str, '%m-%d-%Y')
+        df_dates.append(time)
+
+    df = df.transpose()
+    county_names = df.iloc[0].to_list()
+    df.columns = county_names
+    df = df.iloc[1:]
+    df.insert(0, 'Date', df_dates)
+    df.reset_index(inplace=True, drop=True)
 
     return df
